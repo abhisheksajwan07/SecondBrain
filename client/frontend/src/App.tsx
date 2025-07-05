d90dd53e16e7e91e758ac8d6354b0f3e49d2c359
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-07-05 14:29:10 +0530

    button component ready

--- client/frontend/src/App.tsx
+++ client/frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { PlusIcon } from "./components/icons/PlusIcon";
+import { ShareIcon } from "./components/icons/ShareIcon";
+import { Button } from "./components/ui/button";
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-center">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="flex items-center justify-center h-screen bg-gray-100 gap-4">
+      <Button
+        variant="primary"
+        size="lg"
+        startIcon={<PlusIcon size="lg" />}
+        text="Click me"
+        onClick={() => alert("Button clicked!")}
+      />
+      <Button
+        variant="secondary"
+        size="md"
+        text="share"
+        onClick={() => alert("Share button clicked!")}
+      />
+      <Button
+        variant="secondary"
+        size="sm"
+        text="Share"
+        startIcon={<ShareIcon/>}
+        onClick={() => alert("Share button clicked!")}
+      />
+     
+    </div>
+  );
+};
 
-export default App
+export default App;
--- client/frontend/src/components/ui/button.tsx
+++ client/frontend/src/components/ui/button.tsx
@@ -1,13 +1,34 @@
 import type { ReactElement } from "react";
 
+type Variants = "primary" | "secondary";
 interface ButtonProps {
-    variant:"primary" | "secondary";
-    size:"small" | "medium" | "large";
-    text:string;
-    onClick?: () => void;
-    endIcon?:ReactElement;
-    startIcon?:ReactElement;
+  variant: Variants;
+  size: "sm" | "md" | "lg";
+  text: string;
+  onClick?: () => void;
+  endIcon?: ReactElement;
+  startIcon?: ReactElement;
 }
-export const Button =(props:ButtonProps)=>{
-    return <button></button>
-}
+const variantStyles = {
+  primary: "bg-blue-600 text-white",
+  secondary: "bg-purple-300 text-purple-600",
+};
+const sizeStyles = {
+  sm: "px-3 py-2 text-sm  rounded-sm", 
+  md: "px-4 py-2 text-md rounded-md", 
+  lg: "px-6 py-3 text-lg rounded-lg", 
+};
+const defaultSyles = "rounded-md p-4";
+export const Button = (props: ButtonProps) => {
+  return (
+    <button onClick={props.onClick}
+      className={`${variantStyles[props.variant]} ${defaultSyles} ${
+        sizeStyles[props.size] 
+      } flex items-center gap-2`}
+    >
+      {props.startIcon && <span >{props.startIcon}</span>}
+      <span>{props.text}</span>
+      {props.endIcon}
+    </button>
+  );
+};
