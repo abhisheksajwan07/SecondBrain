c8fd6fbd61d0ccbe0e224e3f57b6469de07fa740
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-07-12 20:43:01 +0530

    profile routes done

--- brainly/src/index.ts
+++ brainly/src/index.ts
@@ -6,6 +6,7 @@ import { connectDB } from "./config/database";
 import authRoutes from "./routes/auth.routes";
 import contentRoutes from "./routes/content.routes";
 import shareRoutes from "./routes/share.routes";
+import profileRoutes from "./routes/profile.routes";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -22,6 +23,7 @@ app.use(
   })
 );
 app.use("/api/v1", authRoutes);
+app.use("api/v1", profileRoutes);
 app.use("/api/v1", contentRoutes);
 app.use("/api/v1", shareRoutes);
 const startServer = async () => {
